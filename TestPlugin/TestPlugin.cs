f7d4ce35e3f1931cf6f35d1004734005d25447fd
Vincent <luckzstuff@gmail.com>
2025-06-13 19:50:41 -0500

    add testplugin and readme

---
+++ TestPlugin/TestPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ServerShared.Plugins;
+
+public class TestPlugin : IPlugin
+{
+    public string Name => "Test Plugin";
+
+    public void Initialize()
+    {
+        Console.WriteLine("[TestPlugin] Initialized!");
+    }
+
+    public void Shutdown()
+    {
+        Console.WriteLine("[TestPlugin] Shutdown!");
+    }
+}
