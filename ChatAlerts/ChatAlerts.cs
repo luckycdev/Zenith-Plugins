fa5c9bc8c5b2912f49ad6c797889c1852c73a449
vincent <50680260+luckycdev@users.noreply.github.com>
2025-07-13 02:12:40 -0500

    Rename BroadcastPlugin to ChatAlerts, use {Name} instead of manually typing plugin name, change command to /alert, change wording and names to match "alert" naming

--- ChatAlerts/ChatAlerts.cs
+++ ChatAlerts/ChatAlerts.cs
@@ -12,26 +12,26 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 
-public class BroadcastConfig
+public class ChatAlertsConfig
 {
-    public string BroadcastPrefix { get; set; } = "[BROADCAST]";
+    public string AlertPrefix { get; set; } = "[ALERT]";
     public int? Color_R { get; set; } = 255;
     public int? Color_G { get; set; } = 0;
     public int? Color_B { get; set; } = 0;
 }
 
-public class BroadcastPlugin : IPlugin
+public class ChatAlerts : IPlugin
 {
-    public string Name => "BroadcastPlugin";
+    public string Name => "ChatAlerts";
 
-    public string Version => "1.2";
+    public string Version => "1.3";
 
     public string Author => "luckycdev";
 
     private string configFilePath;
-    private BroadcastConfig config;
+    private ChatAlertsConfig config;
 
-    public static string broadcastPrefix;
+    public static string alertPrefix;
     public static float rgb_r;
     public static float rgb_g;
     public static float rgb_b;
@@ -45,17 +45,18 @@ public class BroadcastPlugin : IPlugin
         {
             GameServer.Instance.ChatCommands.RegisterCommandsFromAssembly(Assembly.GetExecutingAssembly());
             GameServer.Instance.ConsoleCommands.RegisterCommandsFromAssembly(Assembly.GetExecutingAssembly());
-            commandsRegistered = true; // to stop commands duplicating on plugin reload    
+            commandsRegistered = true; // to stop commands duplicating on plugin reload
+            // TODO: try and make it remove command instead so if reload and the command code changed it will update the command code
         }
         
-        Logger.LogInfo("[BroadcastPlugin] Initialized!");
+        Logger.LogInfo($"[{Name}] Initialized!");
 
         _ = CheckForNewerVersionAsync();
     }
 
     public void Shutdown()
     {
-        Logger.LogInfo("[BroadcastPlugin] Shutdown!");
+        Logger.LogInfo($"[{Name}] Shutdown!");
     }
 
     private void LoadOrCreateConfig()
@@ -65,37 +66,37 @@ public class BroadcastPlugin : IPlugin
 
         if (!File.Exists(configFilePath))
         {
-            config = new BroadcastConfig();
+            config = new ChatAlertsConfig();
             var options = new JsonSerializerOptions { WriteIndented = true };
             var json = JsonSerializer.Serialize(config, options);
             File.WriteAllText(configFilePath, json);
 
-            Logger.LogDebug($"[BroadcastPlugin] Config file created: {configFilePath}");
+            Logger.LogDebug($"[{Name}] Config file created: {configFilePath}");
 
-            Logger.LogWarning($"[BroadcastPlugin] To customize the plugin, check out {configFilePath}");
+            Logger.LogWarning($"[{Name}] To customize the plugin, check out {configFilePath}");
         }
         else
         {
             var json = File.ReadAllText(configFilePath);
-            config = JsonSerializer.Deserialize<BroadcastConfig>(json);
+            config = JsonSerializer.Deserialize<ChatAlertsConfig>(json);
 
-            broadcastPrefix = config.BroadcastPrefix;
+            alertPrefix = config.AlertPrefix;
             rgb_r = config.Color_R.GetValueOrDefault() / 255f;
             rgb_g = config.Color_G.GetValueOrDefault() / 255f;
             rgb_b = config.Color_B.GetValueOrDefault() / 255f;
 
             // check if null or not rgb
-            if (string.IsNullOrWhiteSpace(config.BroadcastPrefix))
-                Logger.LogError($"[BroadcastPlugin] Message in {configFilePath} is invalid!");
+            if (string.IsNullOrWhiteSpace(config.AlertPrefix))
+                Logger.LogError($"[{Name}] Message in {configFilePath} is invalid!");
 
             if (config.Color_R == null || config.Color_R > 255 || config.Color_R < 0)
-                Logger.LogError($"[BroadcastPlugin] Color_R in {configFilePath} is invalid!");
+                Logger.LogError($"[{Name}] Color_R in {configFilePath} is invalid!");
 
             if (config.Color_G == null || config.Color_G > 255 || config.Color_G < 0)
-                Logger.LogError($"[BroadcastPlugin] Color_G in {configFilePath} is invalid!");
+                Logger.LogError($"[{Name}] Color_G in {configFilePath} is invalid!");
 
             if (config.Color_B == null || config.Color_B > 255 || config.Color_B < 0)
-                Logger.LogError($"[BroadcastPlugin] Color_B in {configFilePath} is invalid!");
+                Logger.LogError($"[{Name}] Color_B in {configFilePath} is invalid!");
         }
     }
 
@@ -104,7 +105,7 @@ public class BroadcastPlugin : IPlugin
         try
         {
             using var http = new HttpClient();
-            var fileContent = await http.GetStringAsync("https://raw.githubusercontent.com/luckycdev/Zenith-Plugins/main/BroadcastPlugin/BroadcastPlugin.cs");
+            var fileContent = await http.GetStringAsync("https://raw.githubusercontent.com/luckycdev/Zenith-Plugins/main/Alert/Alert.cs");
 
             var versionMatch = Regex.Match(
                 fileContent,
@@ -121,27 +122,27 @@ public class BroadcastPlugin : IPlugin
                 {
                     if (remoteVersion > localVersion)
                     {
-                        Logger.LogCustom($"[BroadcastPlugin] A newer version is available! Installed: {localVersion}, Latest: {remoteVersion}", ConsoleColor.Blue);
+                        Logger.LogCustom($"[{Name}] A newer version is available! Installed: {localVersion}, Latest: {remoteVersion}", ConsoleColor.Blue);
                     }
                 }
             }
         }
         catch (Exception ex)
         {
-            Logger.LogError($"[BroadcastPlugin] Error checking for new version: {ex}");
+            Logger.LogError($"[{Name}] Error checking for new version: {ex}");
         }
     }
 }
 
-[Command("Broadcast a message to all players in red.", "broadcast", "Broadcast a message.")]
+[Command("Alerts a message to all players.", "alert", "Alerts a message.")]
 [RequireAuth(AccessLevel.Moderator)]
-public class BroadcastChatCommand : ChatCommand
+public class AlertCommand : ChatCommand
 {
     [CommandArgument("Message")]
     public string Message { get; set; }
 
     public override void Handle(string[] args)
     {
-        GameServer.Instance.BroadcastChatMessage($"{BroadcastPlugin.broadcastPrefix} {Message}", new UnityEngine.Color(BroadcastPlugin.rgb_r, BroadcastPlugin.rgb_g, BroadcastPlugin.rgb_b));
+        GameServer.Instance.BroadcastChatMessage($"{ChatAlerts.alertPrefix} {Message}", new UnityEngine.Color(ChatAlerts.rgb_r, ChatAlerts.rgb_g, ChatAlerts.rgb_b));
     }
 }
